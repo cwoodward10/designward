45dc95e775b5750d67d41a1816c3902081766260
Woody_FrameworkWindows <c.woodward10@gmail.com>
2022-04-24 21:29:17 -0700

    initial commit of stuff

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import App from './App.svelte'
+
+import './assets/styles/reset.css';
+import './assets/styles/styles.css';
+
+const app = new App({
+  target: document.getElementById('app')
+})
+
+export default app
