67e6ee7f07f54d99dabd0de66d52dae97b22440f
DESKTOP-B2NAMB7\cwood <c.woodward10@gmail.com>
2022-05-10 22:23:09 -0700

    a couple minor updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,14 +8,15 @@ import { GoToPath } from "$store/RoutingStore";
 
 <main id="main-container" class="mx-auto h-screen w-full flex flex-shrink overflow-hidden flex-grow">
     <article class="m-auto mt-36 card-playful-primary-filled flex flex-col max-w-lg">
-        <h1 class="mb-4">Welcome to Design Ward</h1>
-        <h5>
+        <h1 class="mb-2 text-texton-primary">Welcome to Design Ward</h1>
+        <h6 class="mb-4 text-accent-yellow">Please be aware: This site is under construction.</h6>
+        <p class="text-texton-primary">
             Design Ward is currently a place for me to display my work. 
             You can find that under the Work tab. I would eventually like to 
             include my writing - as well as do much more writing - here. 
             One step at a time though I suppose. For now, check out my work 
             and enjoy this simple web app that I built using Svelte and Tailwind.
-        </h5>
+        </p>
         <div class="flex space-x-2 mt-6">
             <Button 
                 color="{'var(--color-secondary)'}"
