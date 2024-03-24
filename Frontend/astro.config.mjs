efa2fe1dca044eb91103153dc9b8ec01cc185518
woodyframework <c.woodward10@gmail.com>
2024-03-23 21:21:30 -0500

    more setup

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte(), markdoc()],
+  output: "server",
+  adapter: vercel()
+});
