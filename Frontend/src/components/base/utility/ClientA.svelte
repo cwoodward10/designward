3fbd0858b2366c3b903bff91f7df9682c436d91e
woodyframework <c.woodward10@gmail.com>
2024-05-10 16:54:06 -0500

    - update content collection with real content -- fix styles of garden content - update how links animate

--- Frontend/src/components/base/utility/ClientA.svelte
+++ Frontend/src/components/base/utility/ClientA.svelte
@@ -2,16 +2,32 @@
   import { onMount } from "svelte";
 
     export let href: string;
+    export let title: string;
 
     let element: HTMLAnchorElement;
     onMount(() => {
         element.addEventListener('animationend', (e) => {
-            element.classList.remove('initial');
+            element.classList.remove('initial', 'animate');
+        }, {once: true,})
+
+        anchorObserver.observe(element);
+    })
+</script>
+
+<script lang="ts" context="module">
+    const anchorObserver = new IntersectionObserver((entries) => {
+        entries.forEach((e) => {
+            if (e.isIntersecting) {
+                console.log(e)
+                e.target.classList.add('animate');
+
+                anchorObserver.unobserve(e.target);
+            }
         })
     })
 </script>
 
-<a bind:this={element} class="initial" href={href}>
+<a bind:this={element} class="initial" href={href} title={title}>
     <slot/>
 </a>
 
@@ -23,34 +39,37 @@
         color: var(--color-text);
         translate: 0;
         font-weight: 400;
-
+    
+        &::before, &::after {
+            content: "";
+            position: absolute;
+            top: calc(100% + var(--link-underline-offset, 0px));
+            left: 0;
+            
+            height: 2px;
+            width: 0;
+            background-color: var(--color-primary);
+        }
+        &::after {
+            left: unset;
+            right: 0;
+        }
+    }
+    a.initial:global(.animate) {
         animation-name: animate-link;
         animation-duration: var(--link-animation-duration);
         animation-timing-function: ease-out;
         animation-iteration-count: 1;
         animation-fill-mode: forwards;
         animation-delay: calc(var(--initial-animation-delay, 0) + 300ms);
-    
-        &::before, &::after {
-            content: "";
-            position: absolute;
-            top: 100%;
-            left: 0;
-            
-            height: 2px;
-            width: 0;
-            background-color: var(--color-primary);
 
+        &::before, &::after {
             animation-name: animate-link-underline;
             animation-duration: var(--link-underline-animation-duration);
             animation-timing-function: ease-out;
             animation-iteration-count: 1;
             animation-fill-mode: forwards;
             animation-delay: calc(var(--initial-animation-delay, 0) + 250ms);
-        }
-        &::after {
-            left: unset;
-            right: 0;
         }
     }
 </style>
--- Frontend/src/content/config.ts
+++ Frontend/src/content/config.ts
@@ -1,15 +1,25 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
 
+const noteStates = ['concept', 'under construction', 'operational'] as const;
 // 2. Define a `type` and `schema` for each collection
 const thoughtCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-    tags: z.array(z.string()),
+    lastEdited: z.string(),
+    state: z.enum(noteStates),
+    tags: z.array(z.string()).optional(),
     image: z.string().optional(),
   }),
 });
+
+const basicPortfolioCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+
+  })
+})
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
